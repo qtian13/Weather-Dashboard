f1cc827707139bc0a4c7ec01d4c3e690b9e9557c
qtian13 <qiushuang.tian@gmail.com>
2021-07-03 20:47:36 -0700

    add background color to indicate the uvindex scale

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,20 +60,25 @@ var dataDefault = {
 var current = {};
 var cityName = "City Name";
 var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
-if (searchHistory === null) {
+if (searchHistory === null || searchHistory.length === 0) {
     searchHistory = [];
     loadData(dataDefault);
 } else if (searchHistory.length != 0){
     cityName = searchHistory[0];
     fetchWeatherData(cityName);
     for (var i = 0; i < searchHistory.length; i++) {
-        var searchHistoryEl = $("<div>").text(searchHistory[i]);
+        var searchHistoryEl = $("<div>");
+        var searchHistoryTextEl = $("<div>").text(searchHistory[i]);
+        var deleteButton = $("<button>").text("X");
+        searchHistoryEl.addClass("search-history d-flex");
+        searchHistoryTextEl.addClass("search-history-text");
+        deleteButton.addClass("btn delete-button");
         $(".search-history-box").append(searchHistoryEl);
-        searchHistoryEl.addClass("search-history");
+        searchHistoryEl.append(searchHistoryTextEl, deleteButton);
     }
 }
 
-function clickHandler() {
+function searchButtonClickHandler() {
     cityName = $("#search-input").val().replace(/\s+/g, ' ').trim();
     var words = cityName.split(" ");
     $("#search-input").val("");
@@ -86,18 +91,31 @@ function clickHandler() {
 }
 
 function updateSearchHistory(cityToAdd) {
-    $(".search-history").each(function(i) {
-        if ($(this).text() == cityToAdd) {
-            $(this).remove();
+    console.log(cityToAdd);
+    console.log($(".search-history-text"));
+    $(".search-history-text").each(function(i) {
+        if ($(this).text() === cityToAdd) {
+            console.log($(this).text());
+            $(this).parent().remove();
             searchHistory.splice(i, 1);
+            console.log("delete");
             return false;
         }
+        else {
+            console.log($(this).text());
+            console.log("keep");
+        }
     });
-    var searchHistoryEl = $("<div>").text(cityName);
+    var searchHistoryEl = $("<div>");
+    var searchHistoryTextEl = $("<div>").text(cityToAdd);
+    var deleteButton = $("<button>").text("X");
+    searchHistoryEl.addClass("search-history d-flex");
+    searchHistoryTextEl.addClass("search-history-text");
+    deleteButton.addClass("btn delete-button");
     $(".search-history-box").prepend(searchHistoryEl);
-    searchHistoryEl.addClass("search-history");
+    searchHistoryEl.append(searchHistoryTextEl, deleteButton);
     searchHistory.unshift(cityToAdd);
-    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));    
 }
 
 function fetchWeatherData(cityName) {
@@ -116,7 +134,9 @@ function fetchWeatherData(cityName) {
         }
     })
     .then(function(data) {
-        console.log(data);
+        if (data === undefined) {
+            return;
+        }
         current.lon = data.coord.lon;
         current.lat = data.coord.lat;
         requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + current.lat + "&lon=" + current.lon + "&exclude=minutely,hourly&units=imperial&appid=7c84bd2799ba53ed40763cdc11025a8e";
@@ -132,7 +152,9 @@ function fetchWeatherData(cityName) {
             }
         })
         .then(function(data) {
-            console.log(data);
+            if (data === undefined) {
+                return;
+            }
             loadData(data);
         })
     })
@@ -144,20 +166,35 @@ function loadData(data) {
     var humidityEl = $("<div>");
     var windEl = $("<div>");
     var uvIndexEl = $("<div>");
+    var uvIndexNum = $("<span>");
     var imgEl = $("<img>");
+    var momentTime = data.timezone === "x"  ? moment() 
+                                            : moment.unix((moment().valueOf())/1000 + data.timezone_offset);
     current.temp = data.current.temp;
     current.humidity = data.current.humidity;
     current.windSpeed = data.current.wind_speed;
     current.uvIndex = data.current.uvi;
     // current.weather = data.current.weather[0].main;
     current.weatherIcon = data.current.weather[0].icon;
-    var momentTime = data.timezone === "x"  ? moment() 
-                                            : moment.unix((moment().valueOf())/1000 + data.timezone_offset);
     mainInfoEl.text(cityName + " " + momentTime.format("l"));
     tempEl.html("Temperature: " + current.temp + " &#8457");
     humidityEl.text("Humidity: " + current.humidity + "%");
     windEl.text("Wind Speed: " + current.windSpeed + " MPH");
-    uvIndexEl.text("UV Index: " + current.uvIndex);
+    uvIndexEl.text("UV Index: ");
+    uvIndexNum.text(data.current.uvi);
+    uvIndexNum.addClass("uvindex-num");
+    uvIndexEl.append(uvIndexNum);
+    if (data.current.uvi <= 2) {
+        uvIndexNum.css("background-color", "green");
+    } else if (data.current.uvi <= 5) {
+        uvIndexNum.css("background-color", "yellow");
+    } else if (data.current.uvi <= 7) {
+        uvIndexNum.css("background-color", "orange");
+    } else if (data.current.uvi <= 10) {
+        uvIndexNum.css("background-color", "red");
+    } else if (typeof data.current.uvi === "number") {
+        uvIndexNum.css("background-color", "violet");
+    }
     if (current.weatherIcon !== "") {
         imgEl.attr("src", "https://openweathermap.org/img/wn/" + current.weatherIcon + ".png");
     }
@@ -180,10 +217,24 @@ function loadData(data) {
     })
 }
 
-$("#search-button").on("click", clickHandler);
-$(".search-container").on('click', '.search-history', function (event) {
+function removeDuplicate (citySearched) {
+    return citySearched != cityName;
+}
+
+$("#search-button").on("click", searchButtonClickHandler);
+$(".search-container").on('click', '.search-history-text', function (event) {
     cityName = $(event.target).text();
     fetchWeatherData(cityName);
     updateSearchHistory(cityName);
 });
+$(".search-container").on('click', '.delete-button', function (event) {
+    cityName = $(event.target).prev().text();
+    $(event.target).parent().remove();
+    searchHistory = searchHistory.filter(removeDuplicate);
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));  
+    // if (searchHistory.length === 0) {
+    //     loadData(dataDefault);
+    // }  
+});
+
 
